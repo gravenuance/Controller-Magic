a2a09e91b34b696d0b9d09eb8719079edd393429
Markos <markos@mailbox.org>
2026-01-01 13:33:13 +0200

    Added a check to disable multiple instances running at the same time.

--- Controller Magic/Program.cs
+++ Controller Magic/Program.cs
@@ -2,14 +2,23 @@ namespace ControllerMagic
 {
     internal static class Program
     {
+        private static Mutex? _mutex;
         [STAThread]
         static void Main()
         {
+            bool isNew;
+            _mutex = new Mutex(true, "ControllerMagic-69F2B9E1-7C2E-4C11-9C1A-ABCDEF123456", out isNew);
+            if (!isNew)
+            {
+                return;
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
             var context = new TrayApplicationContext();
             Application.Run(context);
+
+            GC.KeepAlive(_mutex);
         }
     }
 }
