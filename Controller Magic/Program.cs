745aebeae9244a7d4f761f5b204164d36b27f4a1
Markos <markos@mailbox.org>
2026-01-01 13:38:29 +0200

    Refactoring Mutex.

--- Controller Magic/Program.cs
+++ Controller Magic/Program.cs
@@ -2,13 +2,11 @@ namespace ControllerMagic
 {
     internal static class Program
     {
-        private static Mutex? _mutex;
+        private static readonly Mutex _mutex = new Mutex(true, "ControllerMagic-69F2B9E1-7C2E-4C11-9C1A-ABCDEF123456", out _);
         [STAThread]
         static void Main()
         {
-            bool isNew;
-            _mutex = new Mutex(true, "ControllerMagic-69F2B9E1-7C2E-4C11-9C1A-ABCDEF123456", out isNew);
-            if (!isNew)
+            if (!_mutex.WaitOne(0, false))
             {
                 return;
             }
@@ -17,8 +15,6 @@ namespace ControllerMagic
 
             var context = new TrayApplicationContext();
             Application.Run(context);
-
-            GC.KeepAlive(_mutex);
         }
     }
 }
